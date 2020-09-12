fb8b4879bf08c36391341ed8ca4b46128841bc1a
Thales Morais <thales.morais21@gmail.com>
2020-09-12 20:37:33 -0300

    Implements .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+console.log(process.env.REACT_APP_API_URL);
 const api = axios.create({
-  baseURL: 'http://192.168.0.108:3333',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
